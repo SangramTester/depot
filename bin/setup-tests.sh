7daaaa2511a48eab5f93d484e26f78128f2dea48
SangramTester <sangram316@yahoo.com>
2020-02-10 23:33:14 -0800

    moving setup and running tests to the same file

--- bin/setup-tests.sh
+++ bin/setup-tests.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 set -exu
 
-echo "Export Sonar scanner version"
-export SONAR_SCANNER_VERSION=4.2.0.1873
+# echo "Export Sonar scanner version"
+# export SONAR_SCANNER_VERSION=4.2.0.1873
 
-echo "set Sonar scanner home"
-export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+# echo "set Sonar scanner home"
+# export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 
-echo "remove Sonar scanner home"
-rm -rf $SONAR_SCANNER_HOME
+# echo "remove Sonar scanner home"
+# rm -rf $SONAR_SCANNER_HOME
 
-echo "create Sonar scanner home"
-mkdir -p $SONAR_SCANNER_HOME
+# echo "create Sonar scanner home"
+# mkdir -p $SONAR_SCANNER_HOME
 
-echo "curl to get Sonar scanner"
-curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+# echo "curl to get Sonar scanner"
+# curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
 
-echo "unzip Sonar scanner"
-unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+# echo "unzip Sonar scanner"
+# unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
 
-echo "remove Sonar scanner zip"
-rm $HOME/.sonar/sonar-scanner.zip
+# echo "remove Sonar scanner zip"
+# rm $HOME/.sonar/sonar-scanner.zip
 
-echo "set Sonar scanner path"
-export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+# echo "set Sonar scanner path"
+# export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 
-echo "set Sonar scanner home"
-export SONAR_SCANNER_OPTS="-server"
+# echo "set Sonar scanner home"
+# export SONAR_SCANNER_OPTS="-server"
 
-echo "test installation========================================================"
-sonar-scanner -h
+# echo "test installation========================================================"
+# sonar-scanner -h
