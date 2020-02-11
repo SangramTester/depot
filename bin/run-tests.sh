847397e305fe9eb474c04f08ec513c64a4d60a65
SangramTester <sangram316@yahoo.com>
2020-02-10 17:34:03 -0800

    Add sonar scanner test

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+set -exu
+
+echo "Running sonar scanner"
+
+sonar-scanner \
+  -Dsonar.projectKey=SangramTester_depot2 \
+  -Dsonar.organization=sangramtester \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io
